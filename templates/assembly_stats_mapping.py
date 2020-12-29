2005cb28affb78b2149280596fe934d05e9f4bba
cimendes <iines.mendes@gmail.com>
2020-12-29 17:18:40 +0000

    debug

--- templates/assembly_stats_mapping.py
+++ templates/assembly_stats_mapping.py
@@ -231,8 +231,9 @@ def get_Lx(alignment_lengths, ref_len, target):
 
     length_so_far = 0
     Lx = 0
-
+    print(Lx)
     for contig_length in sorted_lengths:
+        print(Lx)
         length_so_far += contig_length
         if length_so_far >= target_length:
             Lx += 1
