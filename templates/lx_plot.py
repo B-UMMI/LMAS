7bb76884c3ddb2a304bd97545b882d09ba7ba88b
cimendes <iines.mendes@gmail.com>
2021-02-10 21:53:11 +0000

    Update lx_plot.py

--- templates/lx_plot.py
+++ templates/lx_plot.py
@@ -49,7 +49,7 @@ def main(c90files, l_target):
             fig_Lx = go.Figure()
             i = 0
             for assembler in sorted(df_Lx['Assembler'].unique()):
-                print(df_Lx['nContigs'][(df_Lx['Sample'] == sample) & (df_Lx['Reference'] == reference) & (df_Lx['Assembler'] == assembler)])
+                print(set(df_Lx['nContigs'][(df_Lx['Sample'] == sample) & (df_Lx['Reference'] == reference) & (df_Lx['Assembler'] == assembler)]))
                 if sum(df_Lx['nContigs'][(df_Lx['Sample'] == sample) & (df_Lx['Reference'] == reference) & (df_Lx['Assembler'] == assembler)]) > 0:
                     fig_Lx.add_trace(go.Scatter(x=df_Lx['Lx'][(df_Lx['Sample'] == sample) &
                                                             (df_Lx['Reference'] == reference) &
