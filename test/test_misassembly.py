e351626f268d0b9c6694ccf2c7d1c340c4b16266
cimendes <iines.mendes@gmail.com>
2021-06-04 12:38:54 +0100

    add robustness of tests for chimera, translocation and inversion + translocation

--- test/test_misassembly.py
+++ test/test_misassembly.py
@@ -39,6 +39,7 @@ def not_raises(exception, msg):
     except exception:
         raise pytest.fail(msg)
 
+
 # GLOBAL VARIABLES - TEST FILES
 MISASSEMBLY_PAF_FILE = "data/misassembly/test.paf"
 
@@ -48,8 +49,9 @@ def test_parse_paf():
     test module to parse paf files
     """
     paf_dict = misassembly.parse_paf(MISASSEMBLY_PAF_FILE)
-    assert isinstance(paf_dict,dict)
-    assert len(paf_dict.keys()) == 9
+    assert isinstance(paf_dict, dict)
+    assert len(paf_dict.keys()) == 12
+
 
 def test_filter_dict():
     """
@@ -59,29 +61,55 @@ def test_filter_dict():
     paf_dict = misassembly.parse_paf(MISASSEMBLY_PAF_FILE)
     filter_paf_dict = misassembly.filter_dict(paf_dict)
 
-    assert isinstance(filter_paf_dict,dict)
-    assert len(filter_paf_dict.keys()) == 3
+    assert isinstance(filter_paf_dict, dict)
+    assert len(filter_paf_dict.keys()) == 6
+
 
 def test_classify_misassembled_contigs():
     paf_dict = misassembly.parse_paf(MISASSEMBLY_PAF_FILE)
     filter_paf_dict = misassembly.filter_dict(paf_dict)
-    classified_mis_dict = misassembly.classify_misassembled_contigs(filter_paf_dict)
+    classified_mis_dict = misassembly.classify_misassembled_contigs(
+        filter_paf_dict)
 
-    assert isinstance(classified_mis_dict,dict)
-    assert len(classified_mis_dict.keys()) == 3
+    assert sorted(list(classified_mis_dict.keys())) == [
+        '162', 'contig-100_82', 'Contig_1255_11.5951', 'NODE_188_length_33202_cov_63.293119',  'k141_878', 'scaffold_8']
 
-    assert list(classified_mis_dict.keys()) == ['162', 'NODE_188_length_33202_cov_63.293119',  'k141_878']
-
-    assert classified_mis_dict['162']['misassembly'] == ['inversion', 'translocation']
-    assert classified_mis_dict['NODE_188_length_33202_cov_63.293119']['misassembly'] == ['inversion', 'translocation']
+    # classify chimera
     assert 'chimera' in classified_mis_dict['k141_878']['misassembly'][0]
     assert 'Salmonella_enterica' in classified_mis_dict['k141_878']['misassembly'][0]
     assert 'Escherichia_coli_plasmid' in classified_mis_dict['k141_878']['misassembly'][0]
     assert 'Escherichia_coli' in classified_mis_dict['k141_878']['misassembly'][0]
 
+    assert 'chimera' in classified_mis_dict['Contig_1255_11.5951']['misassembly'][0]
+    assert 'Pseudomonas_aeruginosa' in classified_mis_dict['Contig_1255_11.5951']['misassembly'][0]
+    assert 'Salmonella_enterica' in classified_mis_dict['Contig_1255_11.5951']['misassembly'][0]
 
+    # classify inversion
+
+    # classify translocation
+    assert sorted(classified_mis_dict['scaffold_8']['misassembly']) == [
+        'translocation']
+    assert len(classified_mis_dict['scaffold_8']['strands']) == 1
+    assert any(
+        i > 1000 for i in classified_mis_dict['scaffold_8']['distance_in_ref'])
+
+    # classify inversion + translocation
+    assert sorted(classified_mis_dict['162']['misassembly']) == [
+        'inversion', 'translocation']
+    assert len(classified_mis_dict['162']['strands']) == 2
+    assert any(i > 1000 for i in classified_mis_dict['162']['distance_in_ref'])
+
+    assert sorted(classified_mis_dict['NODE_188_length_33202_cov_63.293119']['misassembly']) == [
+        'inversion', 'translocation']
+    assert len(
+        classified_mis_dict['NODE_188_length_33202_cov_63.293119']['strands']) == 2
+    assert any(
+        i > 1000 for i in classified_mis_dict['NODE_188_length_33202_cov_63.293119']['distance_in_ref'])
+
+
+"""
 def test_all():
-    paf_dict = misassembly.parse_paf("data/misassembly/EHS_GATBMiniaPipeline.paf")
+    paf_dict = misassembly.parse_paf("data/misassembly/EHS_SKESA.paf")
     filter_paf_dict = misassembly.filter_dict(paf_dict)
     classified_mis_dict = misassembly.classify_misassembled_contigs(filter_paf_dict)
 
@@ -89,3 +117,4 @@ def test_all():
 
     with open("misassembly_test.json", "w") as fh:
         fh.write(str(classified_mis_dict))
+"""
